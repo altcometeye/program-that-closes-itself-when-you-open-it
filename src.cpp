8f0155c01958177db260763f7bc1fa48cf26e973
e <ryosukeikhsan@gmail.com>
2026-09-19 09:02:55 +0700

    Add files via upload

---
+++ src.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+wait(int seconds){
+    std::this_thread::sleep_for(std::chrono::milliseconds(s));
+}
+
+int main(){
+    std::cout << "aight cya";
+    wait(1);
+
+    exit(0);
+    
+    return 0;
+}
