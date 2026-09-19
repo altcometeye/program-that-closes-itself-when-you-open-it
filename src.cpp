384a0b6b947da66ef281b9cc0bc0500667eb58e2
altcometeye <ryosukeikhsan@gmail.com>
2026-09-19 09:33:51 +0700

    dih

--- src.cpp
+++ src.cpp
@@ -2,18 +2,26 @@
 #include <chrono>
 #include <thread>
 
-wait(int seconds){
+void wait(int seconds){
     std::this_thread::sleep_for(std::chrono::milliseconds(seconds));
 }
 
 int main(){
     int s;
 
-    std::cout << "how long?";
+    std::cout << "how long? ";
     std::cin >> s;
     std::cout << "aight cya";
 
-    s = (s>10) ? 10 : (s<0) ? 0 : s;
+    if(s>10){
+        s = 10*1000;
+    }
+    else if(s<0){
+        s = 0;
+    }
+    else{
+        s*=1000;
+    }
 
     wait(s);
     exit(0);
